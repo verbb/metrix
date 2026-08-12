1366decc537bdd2622ff168a2eb418f8d953076e
bilyi <a.bilyi@urancompany.com>
2026-08-12 12:10:38 +0300

    Refactor `LastWeek` to use constants and a reusable `getCurrentMonday` method, ensuring cleaner date calculations.

--- src/periods/LastWeek.php
+++ src/periods/LastWeek.php
@@ -12,6 +12,12 @@ use DatePeriod;
 
 class LastWeek extends Period
 {
+    private const MONDAY_THIS_WEEK = 'monday this week 00:00:00';
+    private const ONE_WEEKS_AGO = '-1 weeks';
+    private const TWO_WEEKS_AGO = '-2 weeks';
+    private const THREE_WEEKS_AGO = '-3 weeks';
+    private static ?DateTime $mondayThisWeek = null;
+
     // Static Methods
     // =========================================================================
 
@@ -27,8 +33,10 @@ class LastWeek extends Period
 
     public static function getDateRange(): array
     {
-        $start = new DateTime('monday -2 weeks 00:00:00');
-        $end = new DateTime('monday -1 weeks 00:00:00');
+        $start = self::getCurrentMonday();
+        $start->modify(self::TWO_WEEKS_AGO);
+        $end = self::getCurrentMonday();
+        $end->modify(self::ONE_WEEKS_AGO);
 
         return [
             'start' => $start,
@@ -38,8 +46,10 @@ class LastWeek extends Period
 
     public static function getPreviousDateRange(): array
     {
-        $start = new DateTime('monday -3 weeks 00:00:00');
-        $end = new DateTime('monday -2 weeks 00:00:00');
+        $start = self::getCurrentMonday();
+        $start->modify(self::THREE_WEEKS_AGO);
+        $end = self::getCurrentMonday();
+        $end->modify(self::TWO_WEEKS_AGO);
 
         return [
             'start' => $start,
@@ -54,8 +64,10 @@ class LastWeek extends Period
 
     public static function generatePlotDimensions(WidgetData $widgetData, array $rawData): array
     {
-        $start = new DateTime('monday -2 weeks 00:00:00');
-        $end = new DateTime('monday last week 00:00:00');
+        $start = self::getCurrentMonday();
+        $start->modify(self::TWO_WEEKS_AGO);
+        $end = self::getCurrentMonday();
+        $end->modify(self::ONE_WEEKS_AGO);
 
         $interval = new DateInterval('P1D');
         $period = new DatePeriod($start, $interval, $end);
@@ -75,5 +87,13 @@ class LastWeek extends Period
             'xAxisLabelFormat' => 'datePeriodWeekShort',
             'tooltipFormat' => 'datePeriodWeekLong',
         ];
+    }
+
+    private static function getCurrentMonday(): DateTime {
+        if (!static::$mondayThisWeek instanceof DateTime) {
+            static::$mondayThisWeek = new DateTime(self::MONDAY_THIS_WEEK);
+        }
+
+        return clone static::$mondayThisWeek;
     }
 }
