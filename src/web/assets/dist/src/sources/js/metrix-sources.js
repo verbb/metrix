3a2fe69e75178aa7a392933fda928c3da0201044
Josh Crawford <joshua@sgroup.com.au>
2026-08-25 11:15:47 +1000

    Rebuild front-end assets

--- src/web/assets/dist/src/sources/js/metrix-sources.js
+++ src/web/assets/dist/src/sources/js/metrix-sources.js
@@ -1,2 +1,2 @@
-import{R as i}from"../../../assets/plugin-kit-Cy7J-hQb.js";import"../../../assets/react-vendor-DjYJI7Ii.js";import"../../../assets/lit-vh8rfySC.js";import"../../../assets/plugin-kit-forms-Brt9hHxU.js";import"../../../assets/plugin-kit-core-CIKs8t1s.js";i();
+import{U as i}from"../../../assets/plugin-kit-DPes_Z4D.js";import"../../../assets/react-vendor-DjYJI7Ii.js";import"../../../assets/lit-vh8rfySC.js";import"../../../assets/plugin-kit-forms-Brt9hHxU.js";import"../../../assets/plugin-kit-core-CIKs8t1s.js";i();
 //# sourceMappingURL=metrix-sources.js.map
